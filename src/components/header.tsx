68c735759f2d2344563b65bb87be959028a9506f
MISHUMO TROY LUMADI <59746304+Mishumo-Troy-Lumadi@users.noreply.github.com>
2024-08-09 15:30:36 +0200

    Routing Test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header(){
     return (
-        <div>
+        <div className="w-full h-10 flex bg-neutral-50 items-center justify-center space-x-5">
             <Link to="/auth">Sign In</Link>
             <Link to="/home">Home</Link>
         </div>
